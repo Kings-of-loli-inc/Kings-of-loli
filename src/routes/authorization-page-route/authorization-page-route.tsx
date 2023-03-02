525ca3b4f7c9b3246d90fb3f44074f18ec449b23
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-03-02 22:20:23 +0200

    changed sign up import

--- src/routes/authorization-page-route/authorization-page-route.tsx
+++ src/routes/authorization-page-route/authorization-page-route.tsx
@@ -2,13 +2,13 @@ import { VFC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { LogIn } from '../../modules/log-in/log-in';
-import { SignUp } from '../../modules/sign-in/sign-in';
+import { SignIn } from '../../modules/sign-in/sign-in';
 
 export const AuthorizationPageRoute: VFC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/SignUp" element={<SignIn />} />
         <Route path="/LogIn" element={<LogIn />} />
       </Routes>
     </BrowserRouter>
