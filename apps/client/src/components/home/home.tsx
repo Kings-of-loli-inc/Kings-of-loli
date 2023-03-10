5515e47dfc92f69be061a85d952d0cc5bac552df
Angoliuk <gamongara@gmail.com>
2023-03-10 22:00:24 +0200

    base example added

--- apps/client/src/components/home/home.tsx
+++ apps/client/src/components/home/home.tsx
@@ -4,15 +4,12 @@ import type { RouterOutputs } from '../../trpc';
 
 type HomeProperties = {
   users?: RouterOutputs['users']['getAllUsers'];
-  onUserCreate: () => void;
 };
 
-export const Home: FC<HomeProperties> = ({ users, onUserCreate }) => {
+export const Home: FC<HomeProperties> = ({ users }) => {
   return (
     <>
-      {console.log(users)}
       <div>{users?.map((user) => user.id)}</div>
-      <button onClick={onUserCreate}>Create new User</button>
     </>
   );
 };
