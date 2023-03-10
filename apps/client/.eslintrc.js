090251392da18bcb36f9c9032df76c6752d9ab1f
Angoliuk <gamongara@gmail.com>
2023-03-10 20:45:28 +0200

    configs update

--- apps/client/.eslintrc.js
+++ apps/client/.eslintrc.js
@@ -1,7 +1,13 @@
 /* eslint-disable unicorn/prefer-module */
 module.exports = {
-  extends: ['plugin:react/recommended', 'eslint-config-base/base.js'],
-  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'eslint-config-base/base.js',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+  ],
   settings: {
     react: {
       version: 'detect',
@@ -9,12 +15,13 @@ module.exports = {
   },
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['tsconfig.json'],
+    project: './tsconfig.json',
     sourceType: 'module',
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
   },
 };
--- packages/eslint/base.js
+++ packages/eslint/base.js
@@ -1,23 +1,5 @@
 /* eslint-disable unicorn/prefer-module */
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-    tsconfigRootDir: '../apps',
-    project: ['../apps/*/tsconfig.json'],
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-  },
   env: {
     browser: true,
     amd: true,
@@ -27,16 +9,12 @@ module.exports = {
     'turbo',
     'plugin:jsx-a11y/recommended',
     'plugin:unicorn/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:@typescript-eslint/strict',
     'plugin:prettier/recommended', // Make sure this is always the last element in the array.
   ],
   plugins: ['simple-import-sort', 'prettier', 'check-file'],
   rules: {
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     'jsx-a11y/accessible-emoji': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     'jsx-a11y/anchor-is-valid': [
