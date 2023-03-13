41cb912d198def3b6e745e68353368873156d3cc
Angoliuk <gamongara@gmail.com>
2023-03-13 20:52:38 +0200

    import fix

--- apps/client/src/trpc/index.ts
+++ apps/client/src/trpc/index.ts
@@ -1,7 +1,7 @@
 import { createTRPCReact } from '@trpc/react-query';
 import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server';
 
-import { AppRouter } from '../../../server/src/server';
+import { AppRouter } from '../../../server/src/modules/routes';
 
 export const trpc = createTRPCReact<AppRouter>();
 
